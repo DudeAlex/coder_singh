c9fdec43800cf94a22b482213d3d8b591354ef1a
Dude_Alex <DudeAlexLion@gmail.com>
2021-04-20 18:06:11 +0500

    Initial commit

---
+++ src/main/java/com/springboot/coder_singh/coder_singh/CoderSinghApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.coder_singh.coder_singh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderSinghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderSinghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/coder_singh/coder_singh/CoderSinghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.coder_singh.coder_singh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderSinghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
